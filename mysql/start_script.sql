ec656c4fb32c068aa95036e2199935e7defca307
Nikos Polyzotis <npolizotis@gmail.com>
2024-04-15 13:04:44 +0000

    creating batch schema

--- mysql/start_script.sql
+++ mysql/start_script.sql
@@ -1,2 +1,18 @@
 CREATE USER 'gitpod'@'localhost' IDENTIFIED BY 'gitpod';
 GRANT ALL PRIVILEGES ON *.* TO 'gitpod'@'localhost';
+
+
+CREATE SCHEMA IF NOT EXISTS batching;
+
+CREATE TABLE batching.payments  (
+id VARCHAR(20) NOT NULL PRIMARY KEY,
+details VARCHAR(20) NULL,
+contact VARCHAR(20) NULL,
+location VARCHAR(20) NULL,
+amount NUMERIC(20,3) NOT NULL,
+timestamp timestamp
+);
+
+use mysql;
+
+source /workspace/building-high-throughput-data-microservices/deployments/database/spring-batch-mysql.sql;
